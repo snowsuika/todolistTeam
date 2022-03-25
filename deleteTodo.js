438ba162ca93a33ecbf5cbff0bef602d12b4ac0f
Claire <claire.chang@starclinic.com.tw>
2022-03-25 20:41:04 +0800

    拆分刪除單筆&刪除全部

--- deleteTodo.js
+++ deleteTodo.js
@@ -24,28 +24,25 @@ const errorHandler = res => {
     res.end();
 }
 
-/** 
- * @param {*} type  [ 刪除全部:all | 刪除單筆:single ]
- */ 
-const deleteTodo = (type, res, todos) => {
-    switch (type) {
-        case 'all':
-            todos.length = 0;
-            successHandler(res, todos);
-            break;
-        case 'single':
-            const id = url.split('/').pop();
-            const index = todos.findIndex(item => item.id === id);
-            if (index !== -1) {
-                todos.splice(index, 1);
-                successHandler(res, todos);
-            } else {
-                errorHandler(res);
-            }
-            break;
-        default:
-            break;
+/** 刪除全部 todos */ 
+const deleteAllTodos = (type, res, todos) => {
+    todos.length = 0;
+    successHandler(res, todos);
+}
+
+/** 刪除單筆 todo */
+const deleteSingleTodo = (res, todos) => {
+    const id = url.split('/').pop();
+    const index = todos.findIndex(item => item.id === id);
+    if (index !== -1) {
+        todos.splice(index, 1);
+        successHandler(res, todos);
+    } else {
+        errorHandler(res);
     }
 }
 
-module.exports = deleteTodo;
+module.exports = {
+    deleteAllTodos,
+    deleteSingleTodo
+};
