ebdeb1907b48fca3bdbc9ba8cefa2fd46ca6e29d
kimn <a9523023650@gmail.com>
2022-03-26 18:04:47 +0800

    [新增] sever 增加 get post delete 接口

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require('http');
 const { v4: uuidv4 } = require('uuid');
 const errHandle = require('./errorHandle');
+const getTodo = require('./getTodo')
 const postTodo = require('./postTodo')
+const { deleteAllTodos, deleteSingleTodo } = require('./deleteTodo')
 const todos = [];
 
 const requestListener = async (req, res) => {
@@ -18,15 +20,14 @@ const requestListener = async (req, res) => {
     req['uuidv4'] = uuidv4()
     res['headers'] = headers
 
-
     if (req.url == "/todos" && req.method == "GET") {
-        // getTodo.js
+        getTodo(res, todos)
     } else if (req.url == "/todos" && req.method == "POST") {
         postTodo(req, res, todos)
     } else if (req.url == "/todos" && req.method == "DELETE") {
-        // deleteTodo.js
+        deleteAllTodos('type', res, todos)
     } else if (req.url.startsWith("/todos/") && req.method == "DELETE") {
-        // deleteTodo.js
+        //deleteSingleTodo(res, todos)
     } else if (req.url.startsWith("/todos/") && req.method == "PATCH") {
         // patchTodo.js
     } else if (req.method == "OPTIONS") {
