d4d8a10f4739a2d445aab4fc328e02f847e8f85c
kimn <a9523023650@gmail.com>
2022-03-28 21:40:17 +0800

    格式化

--- patchTodo.js
+++ patchTodo.js
@@ -1,34 +1,34 @@
-const patchTodo = (req,res,todos) =>{
-        const { headers } = res
-        try{
-            const title = JSON.parse(req.body).title;
-            const id = req.url.split('/').pop();
-            const index = todos.findIndex(el => el.id === id);
-            if(title !== undefined && index !== -1){
-                todos[index].title = title
-                res.writeHead(200,headers);
-                res.write(JSON.stringify({
-                    "status":"success",
-                    "data":todos
-                }))
-
-            }else{
-                res.writeHead(400,headers);
-                res.write(JSON.stringify({
-                    "status":"false",
-                    "message":"欄位未填寫正確,或查無此Id"
-                }))
-            }
-            res.end()
-        }catch(err){
-            res.writeHead(400,headers);
+const patchTodo = (req, res, todos) => {
+    const { headers } = res
+    try {
+        const title = JSON.parse(req.body).title;
+        const id = req.url.split('/').pop();
+        const index = todos.findIndex(el => el.id === id);
+        if (title !== undefined && index !== -1) {
+            todos[index].title = title
+            res.writeHead(200, headers);
             res.write(JSON.stringify({
-                "status":"false",
-                "message":err.name +'-'+err.message
+                "status": "success",
+                "data": todos
             }))
-            res.end()
+
+        } else {
+            res.writeHead(400, headers);
+            res.write(JSON.stringify({
+                "status": "false",
+                "message": "欄位未填寫正確,或查無此Id"
+            }))
         }
-    
+        res.end()
+    } catch (err) {
+        res.writeHead(400, headers);
+        res.write(JSON.stringify({
+            "status": "false",
+            "message": err.name + '-' + err.message
+        }))
+        res.end()
+    }
+
 }
 
 
