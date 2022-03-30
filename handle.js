097fc57b9f1a78b2a8fc8228f58f8bd3dfd5bac6
Ed <yide1986@gmail.com>
2022-03-31 00:02:26 +0800

    [修改] errorHandle 與 successHandle 改由參數傳入

--- handle.js
+++ handle.js
@@ -1,11 +1,14 @@
+const library = require('./library');
+const HTTP_STATUS = require('./constants')
+
 // FIXME: 改引用 library.headers
 const errorHandle = (res) => {
-    res.writeHead(400, res.headers);
+    res.writeHead(HTTP_STATUS.BAD_REQUEST.cose, library.headers);
     res.write(JSON.stringify(
         {
             "status": "false",
             // TODO: message 是否改參數傳入，這樣是不是比較靈活。
-            "message": "欄位未填寫正確，或無此 todo ID"
+            "message": HTTP_STATUS.BAD_REQUEST.message
         }
     ));
     res.end();
@@ -13,11 +16,11 @@ const errorHandle = (res) => {
 
 // FIXME: 改引用 library.headers
 const successHandler = (res, todos) => {
-    res.writeHead(200, res.headers);
+    res.writeHead(HTTP_STATUS.OK.code, library.headers);
     res.write(JSON.stringify({
         "status": "success",
         // TODO: message 是否改參數傳入，這樣是不是比較靈活。
-        "message": "刪除成功",
+        "message": HTTP_STATUS.OK.message,
         "data": todos
     }));
     res.end();
