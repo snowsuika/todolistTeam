f8cbd655f796a91e00633428ae8e014632d5b127
Ed <yide1986@gmail.com>
2022-03-31 22:09:13 +0800

    [修改] message 改為參數傳入

--- handle.js
+++ handle.js
@@ -1,26 +1,22 @@
 const library = require('./library');
 const HTTP_STATUS = require('./constants')
 
-// FIXME: 改引用 library.headers
-const errorHandle = (res) => {
+const errorHandle = (res, message) => {
     res.writeHead(HTTP_STATUS.BAD_REQUEST.cose, library.headers);
     res.write(JSON.stringify(
         {
             "status": "false",
-            // TODO: message 是否改參數傳入，這樣是不是比較靈活。
-            "message": HTTP_STATUS.BAD_REQUEST.message
+            "message": message
         }
     ));
     res.end();
 }
 
-// FIXME: 改引用 library.headers
-const successHandler = (res, todos) => {
+const successHandler = (res, todos, message) => {
     res.writeHead(HTTP_STATUS.OK.code, library.headers);
     res.write(JSON.stringify({
         "status": "success",
-        // TODO: message 是否改參數傳入，這樣是不是比較靈活。
-        "message": HTTP_STATUS.OK.message,
+        "message": message,
         "data": todos
     }));
     res.end();
