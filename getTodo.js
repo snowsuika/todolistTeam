35127bcb60f0605b11ef4180095d0d6634461e38
Jerry Huang <Jerry.Huang@dkdtrillion.com>
2022-04-03 00:23:28 +0800

    [優化] getTodo, postTodo, patchTodo

--- getTodo.js
+++ getTodo.js
@@ -1,11 +1,7 @@
+const handle = require('./handle');
+
 const getTodo = (res, todos) => {
-    // TODO: 這裡應該改成用 Handle.successHandler
-    res.writeHead(200, res.headers)
-    res.write(JSON.stringify({
-        "status": "success",
-        "data": todos
-    }));
-    res.end();
+    handle.successHandler(res, todos, '資料讀取成功');
 }
 
 module.exports = getTodo;
--- patchTodo.js
+++ patchTodo.js
@@ -1,4 +1,5 @@
-const { headers } = require("./library")
+const { headers } = require("./library");
+const handle = require('./handle');
 
 const patchTodo = (req, res, todos, body) => {
     try {
@@ -7,30 +8,13 @@ const patchTodo = (req, res, todos, body) => {
         const index = todos.findIndex(el => el.id === id);
         if (title !== undefined && index !== -1) {
             todos[index].title = title
-            // TODO: 這裡應該改成用 Handle.successHandler
-            res.writeHead(200, headers);
-            res.write(JSON.stringify({
-                "status": "success",
-                "data": todos
-            }))
-
+            handle.successHandler(res, todos, '資料更新成功');
         } else {
-            // TODO: 這裡應該改成用 Handle.errorHandle
-            res.writeHead(400, headers);
-            res.write(JSON.stringify({
-                "status": "false",
-                "message": "欄位未填寫正確,或查無此Id"
-            }))
+            handle.errorHandle(res, '欄位未填寫正確,或查無此Id');
         }
         res.end()
     } catch (err) {
-        // TODO: 這裡應該改成用 Handle.errorHandle
-        res.writeHead(400, headers);
-        res.write(JSON.stringify({
-            "status": "false",
-            "message": err.name + '-' + err.message
-        }))
-        res.end()
+        handle.errorHandle(res, `${err.name} + '-' + ${err.message}`);
     }
 
 }
