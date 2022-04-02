ce65c7bcc73f11607061273e7a26515122384a4e
kimn <a9523023650@gmail.com>
2022-04-03 00:02:24 +0800

    [新增] DELETE 測試

--- test/test_query.js
+++ test/test_query.js
@@ -40,3 +40,70 @@ describe('POST /todos 測試', () => {
             });
     });
 });
+
+
+
+
+describe('DELETE /todos 刪除全部 測試', () => {
+    let id = "";
+    // 刪除前先新增一筆資料
+    before((done) => {
+        const postObj = { "title": "新增資料" }
+        api.post('/todos')
+            .send(postObj)
+            .expect(200)
+            .end((err, res) => {
+                // 新增成功取得 id
+                id = res.body.data[0].id
+                done();
+            });
+    });
+
+    it('response.body 必須是物件含有 key : status、data, data 長度為 0', (done) => {
+        api.delete('/todos')
+            .expect(200)
+            .end((err, res) => {
+                assert.notExists(err);
+                assert.hasAllKeys(res.body, ['status', 'data']);
+                expect(res.body.data).to.be.a('array');
+                assert.equal(res.body.data.length, 0);
+                done();
+            });
+    });
+});
+
+describe('DELETE /todos 刪除單筆 測試', () => {
+    const idArray = [];
+    // 刪除前先新增兩筆資料
+    before((done) => {
+        const postObj = { "title": "新增資料1" }
+        api.post('/todos')
+            .send(postObj)
+            .expect(200)
+            .end((err, res) => {
+                idArray.push(res.body.data[0].id)
+            });
+        const postObj2 = { "title": "新增資料2" }
+        api.post('/todos')
+            .send(postObj2)
+            .expect(200)
+            .end((err, res) => {
+                idArray.push(res.body.data[1].id)
+                done();
+            });
+    });
+    it('response.body 必須是吻合測試條件', (done) => {
+        api.delete(`/todos/${idArray[0]}`)
+            .expect(200)
+            .end((err, res) => {
+                assert.notExists(err);
+                assert.hasAllKeys(res.body, ['status', 'data']);
+                expect(res.body.data).to.be.a('array');
+                expect(res.body.data[0]).to.be.a('object');
+                assert.hasAllKeys(res.body.data[0], ['title', 'id']);
+                // 刪除 idArray[0] 後剩餘 idArray[1]
+                assert.equal(res.body.data[0].id, idArray[1]);
+                done();
+            });
+    });
+});
