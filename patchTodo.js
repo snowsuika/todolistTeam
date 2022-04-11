707f372de0bf15e6d950cb14c7fe64866b7a7574
2019wei <linkoko520@gmail.com>
2022-04-11 22:39:29 +0800

    使用 mongoose 的方式改寫patchTodo

--- patchTodo.js
+++ patchTodo.js
@@ -1,13 +1,17 @@
 const handle = require('./handle');
+const Todo = require('./models/todo');
 
-const patchTodo = (req, res, todos, body) => {
+const patchTodo = async (req, res, todos, body) => {
     try {
-        const title = JSON.parse(body).title;
+        // const title = JSON.parse(body).title;
+        const contxt = JSON.parse(body);
         const id = req.url.split('/').pop();
-        const index = todos.findIndex(el => el.id === id);
-        if (title !== undefined && index !== -1) {
-            todos[index].title = title
+        const index = todos.findIndex(el => el._id.toString() === id);
+        if (contxt !== undefined && index !== -1) {
+         await Todo.findByIdAndUpdate(id,contxt).then((todos)=>{
             handle.successHandler(res, todos, '資料更新成功');
+        })  
+            
         } else {
             handle.errorHandle(res, '欄位未填寫正確,或查無此 id');
         }
