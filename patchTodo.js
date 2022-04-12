0aa1d1e3d056483dfc7220f6b6c03de872a53ab6
2019wei <linkoko520@gmail.com>
2022-04-12 21:21:48 +0800

    修正patchTodos

--- patchTodo.js
+++ patchTodo.js
@@ -1,21 +1,14 @@
 const handle = require('./handle');
 const Todo = require('./models/todo');
 
-const patchTodo = async (req, res, todos, body) => {
+const patchTodo = async (req, res, body) => {
     try {
-        // const title = JSON.parse(body).title;
         const contxt = JSON.parse(body);
         const id = req.url.split('/').pop();
-        const index = todos.findIndex(el => el._id.toString() === id);
-        if (contxt !== undefined && index !== -1) {
-         await Todo.findByIdAndUpdate(id,contxt).then((todos)=>{
+        
+        Todo.findByIdAndUpdate(id,contxt).then((todos)=>{
             handle.successHandler(res, todos, '資料更新成功');
         })  
-            
-        } else {
-            handle.errorHandle(res, '欄位未填寫正確,或查無此 id');
-        }
-        res.end()
     } catch (err) {
         handle.errorHandle(res, `${err.name} + '-' + ${err.message}`);
     }
