2425668a6dad6dcf92277020476058efbb973331
2019wei <linkoko520@gmail.com>
2022-04-12 23:30:46 +0800

    修正 PatchTodo

--- patchTodo.js
+++ patchTodo.js
@@ -1,14 +1,14 @@
 const handle = require('./handle');
 const Todo = require('./models/todo');
 
-const patchTodo =  (req, res, body) => {
+const patchTodo = async (req, res, body) => {
     try {
         const contxt = JSON.parse(body);
         const id = req.url.split('/').pop();
         
-        Todo.findByIdAndUpdate(id,contxt).then((todos)=>{
-            handle.successHandler(res, todos, '資料更新成功');
-        })  
+        await Todo.findByIdAndUpdate(id,contxt)
+        const todos = await Todo.find();
+        handle.successHandler(res, todos, '資料更新成功');
     } catch (err) {
         handle.errorHandle(res, `${err.name} + '-' + ${err.message}`);
     }
